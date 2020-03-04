be09f6958d93268a6b2473c0ae3f917e3d7483bc
Amir Daher <amirdaher@live.se>
2020-03-04 20:05:03 +0100

    test

---
+++ coinflip/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo512.png';
+import './App.css';
+
+
+function App() {
+  return (
+   
+    <div className="App">
+
+      <header className="App-header">
+    <p>COINFLIP</p>
+   
+   
+        <img src={logo} className="App-logo" alt="logo" />
+      
+        
+
+      
+     
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
