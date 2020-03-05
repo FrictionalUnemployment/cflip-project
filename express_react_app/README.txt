7572ab8211786b3b92a672563f1878f3fbaac249
Philip Johansson <philipphuket@gmail.com>
2020-03-05 18:56:13 +0100

    finns nu API.txt som förklarar hur man kommunicerar med webbservern.

--- express_react_app/README.txt
+++ express_react_app/README.txt
@@ -4,5 +4,6 @@ app.js är våran frontend. index.js är det som ska köra våran frontend
 för att starta backend skriv 'node server.js' i /express_react_app katalogen
 för att starta frontend skriv 'npm start' i /express_react_app/client katalogen
 
+om crypto inte finns skriv 'npm install crypto --save' i /express_react_app katalogen
 om express inte finns skriv 'npm install express --save' i /express_react_app katalogen
 om frontend inte startar skriv 'npm install' i /experss_react_app/client katalogen
--- express_react_app/server.js
+++ express_react_app/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mariadb = require('mariadb');
+const crypto = require('crypto');
+var bodyParser = require('body-parser');
 
+// Connectar mot våran databas
+let connection;
 mariadb.createConnection({
     user:'coinflip',
     password: 'amirphilip9896',
@@ -9,17 +13,61 @@ mariadb.createConnection({
     })
     .then(conn => {
         console.log("Connected to database. Connection id is " + conn.threadId);
+        connection = conn;
     })
     .catch(err => {
         console.log('error connecting to database: ' + err);
     });
 
 
+// Startar waebservern och lyssnar
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 const port = process.env.PORT || 5000; // Om port inte sätts när man startar så är den 5000
-
 app.listen(port, () => console.log('Listening on port ' + port));
 
-app.get('/test_get', (req, res) => {
-    res.send({express: 'test_get successful'});
+// ==========================================================
+// Här finns alla API som går att nå från frontend
+
+// Lägg till användare
+app.post('/register_user', (req, res) => {
+    const pwd = String(req.body.password);
+    const user = String(req.body.username);
+
+    const hash = crypto.createHash('sha256')
+                    .update(pwd)
+                    .digest('hex');
+    connection.query(`INSERT INTO user (Username, Password, Balance) VALUES ("${user}", "${hash}", 50);`)
+                .then(ans => {
+                    res.send({express: user});
+                })
+                .catch(err => {
+                    console.log('error registering user:' + err);
+                })
 })
+
+// Logga in användare
+app.post('/login', (req, res) => {
+    const pwd = String(req.body.password);
+    const user = String(req.body.username);
+
+    const hash = crypto.createHash('sha256')
+                    .update(pwd)
+                    .digest('hex');
+    connection.query(`SELECT Password FROM user WHERE Username="${user}"`)
+                .then(ans => {
+                    let stored_hash = ans[0].Password;
+                    if (stored_hash === hash) {
+                        res.send({express: user});
+                        console.log('logged in');
+                    } else {
+                        res.send({express: 'false'});
+                        console.log('not loggin in');
+                    }
+                })
+                .catch(err => {
+                    console.log('error logging in: ' + err);
+                })
+})
+
