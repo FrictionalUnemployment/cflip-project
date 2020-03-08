04ba63a87bdac001c0cfc78bc574b7eacfa46f42
Alexander Norberg <alexander.norberg@student.hv.se>
2020-03-08 19:17:07 +0100

    Påbörjat med bet-funktion.

--- express_react_app/client/src/App.js
+++ express_react_app/client/src/App.js
@@ -38,12 +38,58 @@ class TopList extends React.Component {
     }
 }
 
+class BetChoice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            amount: 0
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.onClick(this.props.suit, this.state.amount);
+    }
+
+    render() {
+        return (
+            <button className="s" onClick={this.handleClick}>Bet {this.props.suit}!</button>
+        )
+    }
+}
+
 class Game extends Component {
+
+    placeBet = async (suit, amount) => {
+        // Här sätts vad, vem och hur mycket
+        const data = {bet: suit, username: "test_user", amount: amount};
+
+        const response = await fetch('/place_bet', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {'Accept':'application/json', 'Content-Type': 'application/json'}
+        });
+
+        const body = await response.json();
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body.express;
+    }
+
+    handleClick(suit, amount) {
+        let res = this.placeBet(suit, amount);
+        console.log(res);
+
+    }
+
     render() {
         return (
             <div className="App-game">
                 <div>
+                    <BetChoice suit="Heads" onClick={this.handleClick} />
                     <img src={logo} className="App-logo" alt="logo" />
+                    <BetChoice suit="Tails" onClick={this.handleClick} />
                 </div>
             </div>
         );
