6a66b433bb4d0c8292f26f1afa297f29b6aaffca
Philip Johansson <philipphuket@gmail.com>
2020-03-13 19:36:26 +0100

    ändrat färg på captcha

--- server/routers/captcha.js
+++ server/routers/captcha.js
@@ -7,7 +7,7 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-    let captcha = svgCaptcha.create({ noise: 4, size: 5 });
+    let captcha = svgCaptcha.create({ noise: 4, size: 5, background: '#282c34' });
     req.session.captcha = captcha.text;
     res.type('svg');
     res.send(captcha.data);
