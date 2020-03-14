69d5304e7ce441f67b2034900a2c834d7502346a
Alexander Norberg <alexander.norberg@student.hv.se>
2020-03-14 21:30:50 +0100

    Fixade lite formatering.

--- client/src/components/loginpopup.js
+++ client/src/components/loginpopup.js
@@ -1,77 +1,78 @@
-import React from 'react';  
-import './style.css';  
-import ReactHtmlParser from 'react-html-parser'; 
-class Loginpopup extends React.Component {  
-
-  constructor(props){
-    super(props)
-      this.state = {svgData: ''}
-  }
-
-  componentDidMount(){
-    this.getCaptcha();
+import React from "react";
+import "./style.css";
+import ReactHtmlParser from "react-html-parser";
+class Loginpopup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { svgData: "" };
     }
 
-  handleChange = event => {
-    event.preventDefault()
-    this.props.handleOnChange(event)
+    componentDidMount() {
+        this.getCaptcha();
+    }
+
+    handleChange = event => {
+        event.preventDefault();
+        this.props.handleOnChange(event);
+    };
+
+    getCaptcha = async () => {
+        const response = await fetch("/captcha");
+        const data = await response.text();
+
+        this.setState({ svgData: data });
+    };
+
+    render() {
+        return (
+          <div className="popup">
+            <div className="popup\_inner">
+              <button onClick={this.props.closeLoginPopup}>X</button>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  border: "1px solid white"
+                }}
+              >
+                <div>
+                  <h2>{this.props.text}</h2>
+                  <form onSubmit={e => e.preventDefault()}>
+                    <input
+                      placeholder="Enter username"
+                      type="text"
+                      name="username"
+                      onChange={this.handleChange}
+                    />
+                    <br />
+
+                    <input
+                      type="password"
+                      placeholder="Enter password"
+                      name="password"
+                      onChange={this.handleChange}
+                    />
+                    <br />
+                    <div> {ReactHtmlParser(this.state.svgData)} </div>
+                    <div style={{ display: "flex" }}>
+                      <input
+                        type="text"
+                        name="captcha"
+                        placeholder="Enter captcha code"
+                        onChange={this.handleChange}
+                      />
+                      <br />
+                      <button onClick={this.props.handleLoginSubmit}>
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
 }
-
-getCaptcha = async () => {  
-  const response = await fetch('/captcha')
-  const data = await response.text();
- 
-  this.setState({svgData: data });
-}
-
-
-render() {
-return(
-    <div className='popup'>  
-    <div className='popup\_inner'> 
-    <button onClick={this.props.closeLoginPopup}>X</button>  
-    <div style={{display: 'flex', justifyContent: 'center', border: '1px solid white'}}>
-    <div>
-    <h2>{this.props.text}</h2>
-    <form onSubmit={e => e.preventDefault()}>
-    <input 
-    placeholder="Enter username"  
-    type="text" 
-    name="username"
-    onChange={this.handleChange}
-    /><br />
-
-    <input type="password" 
-    placeholder="Enter password" 
-    name="password"
-    onChange={this.handleChange}
-    /><br />
-     <div> { ReactHtmlParser (this.state.svgData) } </div>
-     <div style={{display: 'flex'}}>
-    <input
-    type="text"
-    name="captcha"
-    placeholder="Enter captcha code"
-    onChange={this.handleChange}
-    /><br/>
-     <button 
-    onClick={this.props.handleLoginSubmit}>
-     Submit
-    </button></div>
-   
-
-    </form>
-    </div>
-    </div>
-    </div>
-    </div>
-
-
-);
-
-}
-}
-
 
 export default Loginpopup;
-
