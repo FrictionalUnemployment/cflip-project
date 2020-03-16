dab5ac82de0feac765204678e1144f60d095805b
Amir Daher <amirdaher@live.se>
2020-03-16 23:01:02 +0100

    hi

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,6 +1,7 @@
 import React from 'react';  
 import './style.css';  
 import {Table} from "reactable";
+import { getQueriesForElement } from '@testing-library/react';
 
 
 
@@ -10,13 +11,26 @@ class Statistics extends React.Component {
         super(props);
         this.state = {
             info: '',
-            displayStats: 'top',
-            
+            filterUsername: '',
+            array: [],
+            userInfo: '',
+            winsInfo: '',
+            showStats: false,
+            userArray: []
+        };
 
-        }
     }
+
+    handleOnChange(event) {
+       
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+
+    }
+
     async componentDidMount() {
-        const response = await fetch('/stats/toplist/' + this.state.displayStats + '/25')
+        const response = await fetch('/stats/toplist/top/10000')
         const body = await response.json();
         
         if(response.status !== 200) {
@@ -29,18 +43,75 @@ class Statistics extends React.Component {
         });
 
     }
-    handleChange = event => {
-        event.preventDefault()
-        this.props.handleOnChange(event)
+
+     async getUser() {
+         
+        const response = await fetch('/stats/user/' + this.state.filterUsername);
+        const body = await response.json();
+
+        if(response.status !== 200) {
+            throw Error(body.message);
+        }
+        
+        this.setState({
+            userInfo: body
+        });
+      }
+
+      async getWID() {
+          const userInfo = this.state.userInfo;
+          
+        for(var i=0; i<userInfo.Wins.length; i++){
+            
+        const response = await fetch('/stats/flip/' + userInfo.Wins[i]);
+   
+        const body = await response.json();
+        if(response.status !== 200) {
+            throw Error(body.message);
+        }
+        this.setState({
+            winsInfo: body, showStats: true
+        });
+    
     }
+      }
+
+      handleUserQuery() {
+          
+        this.getUser()
+            .then(() => this.getWID());
+      }
+
+      showUserStats() {
+          
+        const userInfo = this.state.userInfo;
+        const userWID = this.state.winsInfo;
+        alert(userWID.results)
+        for (var i=0; i<userInfo.Wins.length; i++) {
+            
+            var obj = {
+                "Username": userInfo.Username,
+                "Balance": userInfo.Balance,
+                "Wins": toString(userWID)
+                
+            };
+            this.state.userArray.push(obj);
+        }
+      }
+
+   
+    
+      filterArray = e => {
+       // this.setState({filterUsername: e.target.value});
+    }
+        
+            
+    
 
     render() {
-     
-        var info = this.state.info;
-        var array = [];
         
+        const info = this.state.info;
         for (var i=0; i<info.length; i++) {
-            console.log(info[i].Username);
             var obj = {
                 "#": i+1,
                 "Username": info[i].Username,
@@ -48,16 +119,15 @@ class Statistics extends React.Component {
                 "Wins": info[i].Wins,
                 "Losses": info[i].Losses 
             };
-           
-            array.push(obj);
+            this.state.array.push(obj);
         }
-        
 
         return(
             <div className='popup'>  
-            
-            
-            <Table className="table" sortable={true} data={array} />
+                    <Table className="table" sortable={true} data={this.state.array} itemsPerPage={10}
+                    pageButtonLimit={10}  filterable={["Username"]} onFilter={this.filterArray}
+                    
+                    />
             </div>
            
            
