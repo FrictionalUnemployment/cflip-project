2bf0d1d5ec1b610e6373956cbd91dfb51afac599
Amir Daher <amirdaher@live.se>
2020-03-20 14:52:09 +0100

    Hi

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './style.css';
 import ReactTable from 'react-table-6'
@@ -6,15 +5,6 @@ import 'react-table-6/react-table.css'
 
 
 class Statistics extends React.Component {
-=======
-import React from 'react';  
-import './style.css';  
-import {Table} from "reactable";
-// v Denna används ej? Kommenterar ut den.
-//import { getQueriesForElement } from '@testing-library/react';
-
-class Statistics extends React.Component {  
->>>>>>> 4e602aae4bcb3d2e009b3d0a027772834819d45b
     constructor(props) {
         super(props);
         this.state = {
