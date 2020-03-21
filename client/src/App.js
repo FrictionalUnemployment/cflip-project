da07d59c0133102c23242d043b95390dce5bd05f
Alexander Norberg <alexander.norberg@student.hv.se>
2020-03-21 18:35:22 +0100

    Tog bort lite varningar, bl.a var->let. och useless string concat.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js'
 import Game from './components/Game.js'
-import Statistics from './components/Statistics';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
